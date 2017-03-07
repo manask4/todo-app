f3cf9b768e991f660caaefc0161ad541928ff4a3
manask4 <makk48@rocketmail.com>
2017-03-07 22:19:13 +0530

    indentation and serviceworker update

--- js/main.js
+++ js/main.js
@@ -1,89 +1,113 @@
-$(document).ready(function() {
+$(function() {
     $('main').slideDown(300).animate({'opacity': 1}, {'queue': false, 'duration': 300});
     $('footer').fadeIn(1200);
-});
 
-var storageData = JSON.parse(localStorage.getItem("notes"));
-if (!$.isEmptyObject(storageData)) {
-    var allNotes = '';
-    for (var day in storageData) {
-        var string = '<div class="note-section"><span class="day">' + day + '</span>';
-        var note = '';
-        for (var uid in storageData[day]) {
-            var text = storageData[day][uid]['note'];
-            var time = storageData[day][uid]['time'];
-            var noteString = '<span class="note-text">' + text + '</span><span class="time">' + time + '</span>';
-            var wellWrapper = wrap_notes(noteString, uid);
-            note += wellWrapper;
+    // get data stored in localStorage and display it on page load.
+    var storageData = JSON.parse(localStorage.getItem("notes"));
+    if (!$.isEmptyObject(storageData)) {
+        var allNotes = '';
+        for (var day in storageData) {
+            var string = '<div class="note-section"><span class="day">' + day + '</span>';
+            var note = '';
+            for (var uid in storageData[day]) {
+                var text = storageData[day][uid].note;
+                var time = storageData[day][uid].time;
+                var noteString = '<span class="note-text">' + text + '</span><span class="time">' + time + '</span>';
+                var wellWrapper = wrapNotes(noteString, uid);
+                note += wellWrapper;
+            }
+            var dayNotes = string + note + '</div>'; 
+            allNotes += dayNotes;
         }
-        var dayNotes = string + note + '</div>'; 
-        allNotes += dayNotes;
+        $('.panel-body').show();
+        $('.user-notes').append(allNotes);
     }
-    $('.panel-body').show();
-    $('.user-notes').append(allNotes);
-}
 
-$('#input-note').keypress(function(e) {
-    if(e.which == 13) {
-        $('#add-note-btn').trigger('click');
-    }
-});
+    // enter button is 'add'
+    $('#input-note').keypress(function(e) {
+        if(e.which == 13) {
+            $('#add-note-btn').trigger('click');
+        }
+    });
 
-$('#add-note-btn').click(function(e) {
-    var inputVal = $('#input-note').val();
-    if (inputVal) {
-        if (typeof(localStorage) !== "undefined") {
-            var notesStorage = localStorage.getItem("notes");
-            var uniqueId = get_random_id();
-            if (notesStorage) {
-                var data = JSON.parse(notesStorage);
-                update_localstorage_data(inputVal, data, uniqueId);
+    // get the input and store it in localStorage
+    $('#add-note-btn').click(function(e) {
+        var inputVal = $('#input-note').val();
+        if (inputVal) {
+            if (typeof(localStorage) !== "undefined") {
+                var notesStorage = localStorage.getItem("notes");
+                var uniqueId = getRandomId();
+                if (notesStorage) {
+                    var data = JSON.parse(notesStorage);
+                    updateLocalstorageData(inputVal, data, uniqueId);
+                }
+                else {
+                    setLocalstorageData(inputVal, uniqueId);
+                }
+                appendNotes(inputVal, uniqueId);
+                $('.panel-body').show();
             }
             else {
-                set_localstorage_data(inputVal, uniqueId);
+                $(".panel").effect("shake");
             }
-            append_notes(inputVal, uniqueId);
-            $('.panel-body').show();
+            $('#input-note').val('').focus();
         }
         else {
-            $(".panel").effect("shake");
+            $('#input-note').effect("highlight", {color: '#ef9a9a'}, 500).focus();
         }
-        $('#input-note').val('').focus();
-    }
-    else {
-        $('#input-note').effect("highlight", {color: '#ef9a9a'}, 500).focus();
-    }
+    });
+
+    // delete functionality
+    $('.user-notes').on('click', '.trash-icon', function() {
+        var noteElement = $(this).closest('.notes-list');
+        var thisElement = $(this);
+        deleteNote(thisElement, noteElement);
+    });
+
+    // checked functionality
+    $('.user-notes').on('click', '.check-icon', function() {
+        var id = $(this).attr('id').substring(4);
+        var day = $(this).closest('.notes-list').siblings('.day').text();
+        var notesStorage = JSON.parse(localStorage.getItem("notes"));
+        if ($(this).hasClass('fa-check-circle-o')) {
+            $(this).closest('.well').animate({'background-color':'#f7f2c5'}, 300);
+            $(this).addClass('fa-check-circle').removeClass('fa-check-circle-o');
+            var timeStamp = $.now();
+            notesStorage[day][id].checked = timeStamp;
+            localStorage.setItem("notes", JSON.stringify(notesStorage));
+        }
+        else {
+            $(this).closest('.well').animate({'background-color':'white'}, 300);
+            $(this).addClass('fa-check-circle-o').removeClass('fa-check-circle');
+            delete notesStorage[day][id].checked;
+            localStorage.setItem("notes", JSON.stringify(notesStorage));
+        }
+    });
+
+    // delete items which are checked for 5 minutes.
+    setInterval(function() {
+        var currentTime = $.now();
+        var notesStorage = JSON.parse(localStorage.getItem("notes"));
+        if (!$.isEmptyObject(notesStorage)) {
+            for (var day in notesStorage) {
+                for (var id in notesStorage[day]) {
+                    if (notesStorage[day][id].hasOwnProperty('checked')) {
+                        var checkedTime = notesStorage[day][id].checked;
+                        if (((currentTime - checkedTime)/1000/60) >= 5) {
+                            notesStorage = animateDelete(notesStorage, id, day, 'pulsate');
+                        }
+                    }
+                }
+            }
+            localStorage.setItem("notes", JSON.stringify(notesStorage));
+        }
+    }, 5000);
 });
 
-$('.user-notes').on('click', '.trash-icon', function() {
-    var noteElement = $(this).closest('.notes-list');
-    var thisElement = $(this);
-    delete_note(thisElement, noteElement);
-});
-
-$('.user-notes').on('click', '.check-icon', function() {
-    var id = $(this).attr('id').substring(4);
-    var day = $(this).closest('.notes-list').siblings('.day').text();
-    var notesStorage = JSON.parse(localStorage.getItem("notes"));
-    if ($(this).hasClass('fa-check-circle-o')) {
-        $(this).closest('.well').animate({'background-color':'#f7f2c5'}, 300);
-        $(this).addClass('fa-check-circle').removeClass('fa-check-circle-o');
-        var timeStamp = $.now();
-        notesStorage[day][id]['checked'] = timeStamp;
-        localStorage.setItem("notes", JSON.stringify(notesStorage));
-    }
-    else {
-        $(this).closest('.well').animate({'background-color':'white'}, 300);
-        $(this).addClass('fa-check-circle-o').removeClass('fa-check-circle');
-        delete notesStorage[day][id]['checked'];
-        localStorage.setItem("notes", JSON.stringify(notesStorage));
-    }
-});
-
-function append_notes(note, uniqueId) {
-    var datetime = get_current_datetime();
+function appendNotes(note, uniqueId) {
+    var datetime = getCurrentDatetime();
     note = '<span class="note-text">' + note + '</span><span class="time">' + datetime.time + '</span>';
-    var wellWrapper = wrap_notes(note, uniqueId);
+    var wellWrapper = wrapNotes(note, uniqueId);
     var day = datetime.day;
     var dayPresent = $('span:contains('+day+')');
     if (dayPresent.length == 1) {
@@ -95,26 +119,25 @@ function append_notes(note, uniqueId) {
     }
 }
 
-function wrap_notes(note, uniqueId) {
-    if (is_checked_note(uniqueId)) {
+function wrapNotes(note, uniqueId) {
+    if (isCheckedNote(uniqueId)) {
         return '<div style="background-color:#f7f2c5" class="notes-list well well-sm">' + note + '<span class="util-icons"><i id="del-'+uniqueId+'" class="trash-icon fa fa-trash-o" aria-hidden="true"></i><i id="che-'+uniqueId+'" class="check-icon fa fa-check-circle" aria-hidden="true"></i></span></div>';
     }
     else {
         return '<div class="notes-list well well-sm">' + note + '<span class="util-icons"><i id="del-'+uniqueId+'" class="trash-icon fa fa-trash-o" aria-hidden="true"></i><i id="che-'+uniqueId+'" class="check-icon fa fa-check-circle-o" aria-hidden="true"></i></span></div>';
     }
-
 }
 
-function set_localstorage_data(note, uniqueId) {
-    datetime = get_current_datetime();
-    var notes_storage_data = {};
-    notes_storage_data[datetime.day] = {};
-    notes_storage_data[datetime.day][uniqueId] = {'note': note, 'time': datetime.time};
-    localStorage.setItem("notes", JSON.stringify(notes_storage_data));
+function setLocalStorageData(note, uniqueId) {
+    datetime = getCurrentDatetime();
+    var notesStorageData = {};
+    notesStorageData[datetime.day] = {};
+    notesStorageData[datetime.day][uniqueId] = {'note': note, 'time': datetime.time};
+    localStorage.setItem("notes", JSON.stringify(notesStorageData));
 }
 
-function update_localstorage_data(note, data, uniqueId) {
-    datetime = get_current_datetime();
+function updateLocalstorageData(note, data, uniqueId) {
+    datetime = getCurrentDatetime();
     if (data[datetime.day]) {
         data[datetime.day][uniqueId] = {'note': note, 'time': datetime.time};
         localStorage.setItem("notes", JSON.stringify(data));
@@ -126,7 +149,7 @@ function update_localstorage_data(note, data, uniqueId) {
     }
 }
 
-function get_current_datetime() {
+function getCurrentDatetime() {
     var dt = new Date();
     var day = dt.toDateString();
     var hours = dt.getHours();
@@ -137,19 +160,19 @@ function get_current_datetime() {
     return {day: day, time: time};
 }
 
-function delete_note(element, noteElement) {
+function deleteNote(element, noteElement) {
     var id = $(element).attr('id').substring(4);
     var day = $(noteElement).siblings('.day').text();
     var notesStorage = JSON.parse(localStorage.getItem("notes"));
-    notesStorage = animate_delete(notesStorage, id, day, 'drop');
+    notesStorage = animateDelete(notesStorage, id, day, 'drop');
     localStorage.setItem("notes", JSON.stringify(notesStorage));
 }
 
-function get_random_id() {
+function getRandomId() {
     return (Math.random() + 1).toString(16).substring(2,7);
 }
 
-function is_checked_note(uid) {
+function isCheckedNote(uid) {
     var notesStorage = JSON.parse(localStorage.getItem("notes"));
     for (var day in notesStorage) {
         for (var id in notesStorage[day]) {
@@ -163,25 +186,7 @@ function is_checked_note(uid) {
     return false;
 }
 
-setInterval(function() {
-    var currentTime = $.now();
-    var notesStorage = JSON.parse(localStorage.getItem("notes"));
-    if (!$.isEmptyObject(notesStorage)) {
-        for (var day in notesStorage) {
-            for (var id in notesStorage[day]) {
-                if (notesStorage[day][id].hasOwnProperty('checked')) {
-                    var checkedTime = notesStorage[day][id]['checked'];
-                    if (((currentTime - checkedTime)/1000/60) >= 5) {
-                        notesStorage = animate_delete(notesStorage, id, day, 'pulsate');
-                    }
-                }
-            }
-        }
-        localStorage.setItem("notes", JSON.stringify(notesStorage));
-    }
-}, 5000);
-
-function animate_delete(notesStorage, id, day, effectType) {
+function animateDelete(notesStorage, id, day, effectType) {
     delete notesStorage[day][id];
     if ($.isEmptyObject(notesStorage[day])) {
         delete notesStorage[day];
--- sw.js
+++ sw.js
@@ -1,28 +1,30 @@
 importScripts('cache-polyfill.js');
 
 self.addEventListener('install', function(e) {
- e.waitUntil(
-   caches.open('todo').then(function(cache) {
-     return cache.addAll([
-       '/todo-app/',
-       '/todo-app/index.html',
-       '/todo-app/css/main.css',
-       '/todo-app/css/font-awesome.min.css',
-       '/todo-app/js/main.js',
-     ]);
-   })
- );
+  self.skipWaiting();
+  e.waitUntil(
+    caches.open('todo').then(function(cache) {
+      return cache.addAll([
+        '/todo-app/',
+        '/todo-app/index.html',
+        '/todo-app/css/main.css',
+        '/todo-app/css/font-awesome.min.css',
+        '/todo-app/js/main.js',
+      ]);
+    })
+  );
 });
 
 self.addEventListener('fetch', function(event) {
-    event.respondWith(
-        fetch(event.request).then(function(response) {
-            caches.open('todo').then(function(cache) {
-                cache.put(event.request, response);
-            });
-            return response.clone();
-        }).catch(function() {
-            return caches.match(event.request);
-        })
-    );
+  event.respondWith(
+    caches.open('todo').then(function(cache) {
+      return cache.match(event.request).then(function(response) {
+        var fetchPromise = fetch(event.request).then(function(response) {
+          cache.put(event.request, response.clone());
+          return response;
+        });
+        return response || fetchPromise;
+      });
+    })
+  );
 });
